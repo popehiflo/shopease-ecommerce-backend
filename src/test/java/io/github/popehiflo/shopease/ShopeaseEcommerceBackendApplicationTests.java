435315831fb6e3d0280d150213a69cd6581f5f6c
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2024-10-27 15:04:26 -0500

    feat: initialize project (backend) with spring initializr

---
+++ src/test/java/io/github/popehiflo/shopease/ShopeaseEcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.popehiflo.shopease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeaseEcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
